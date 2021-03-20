48b45551b415c75a63fa0b15a9a69e5077dd0241
unknown65182 <unknown65182@gmail.com>
2021-03-21 00:35:56 +0300

    add lodash & add unique id to checkbox

--- src/components/CheckboxButton/index.jsx
+++ src/components/CheckboxButton/index.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import _uniqueId from "lodash/uniqueId";
 import { Wrapper, Icon, Label } from "./index.styled";
 
 const CheckboxButton = ({ actived, label }) => {
+  const [id] = useState(() => _uniqueId("checkbox_id_"));
   const [active, setActive] = useState(actived ?? false);
   const handleCheck = () => {
     setActive(!active);
   };
+
   return (
     <Wrapper>
       <Icon
         checked={active}
         type="checkbox"
-        id="checkbox-button"
+        id={id}
         onChange={() => handleCheck()}
       />
-      <Label htmlFor="checkbox-button">{label}</Label>
+      <Label htmlFor={id}>{label}</Label>
     </Wrapper>
   );
 };
